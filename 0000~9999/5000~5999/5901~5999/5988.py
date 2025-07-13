e7c01fd880b339a18102735e9ec878fced0d96b0
seungyeon <seongyon98@gmail.com>
2025-07-13 16:06:37 +0900

    create 5988, 34052

---
+++ 0000~9999/5000~5999/5901~5999/5988.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    if int(input()) % 2 == 0:
+        print("even")
+    else:
+        print("odd")
---
+++ 30000~39999/34000~34999/34000~34100/34052.py
@@ -0,0 +1,2 @@
+t = [int(input()) for _ in range(4)]
+print("Yes") if sum(t) <= 1500 else print("No")
