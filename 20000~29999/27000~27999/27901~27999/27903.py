72feed8eae6515f0a64a5bdc98b9f20774655953
seungyeon <seongyon98@gmail.com>
2025-08-06 21:44:37 +0900

    create 27903, 34073

---
+++ 20000~29999/27000~27999/27901~27999/27903.py
@@ -0,0 +1,12 @@
+print(
+    chr(115) +  # s
+    chr(101) +  # e
+    chr(111) +  # o
+    chr(110) +  # n
+    chr(103) +  # g
+    chr(121) +  # y
+    chr(111) +  # o
+    chr(110) +  # n
+    chr(57)  +  # 9
+    chr(56)     # 8
+)
---
+++ 30000~39999/34000~34999/34000~34100/34073.py
@@ -0,0 +1,6 @@
+int(input())
+s = list(input().split())
+for i in range(len(s)):
+    s[i] += "DORO"
+    
+print(" ".join(s))
